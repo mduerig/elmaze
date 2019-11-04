0bbc61038dbbe9b71b6c73ef353e47aa622377a5
Michael Dürig <michid@gmail.com>
2019-11-04 22:31:13 +0100

    inject init and update functions into game

---
+++ src/Game.elm
@@ -0,0 +1,619 @@
+module Game exposing (..)
+
+import Array exposing (..)
+import Browser exposing (document)
+import Browser.Events exposing ( onKeyDown, onKeyUp)
+import Collage exposing (..)
+import Collage.Layout exposing (..)
+import Collage.Render exposing (svg)
+import Collage.Text as Text
+import Color exposing (..)
+import Html exposing (Html)
+import Html.Attributes as Atts
+import Html.Events exposing ( onClick, onInput )
+import Json.Decode as Decode
+import Time
+
+
+type alias Game solver =
+    { board : Board
+    , mode : Mode
+    , editor : Editor
+    , programmer : Programmer
+    , executor : Executor solver
+    }
+
+type Move
+    = Forward
+    | TurnLeft
+    | TurnRight
+    | Nop
+
+type alias Board =
+    { width : Int
+    , height : Int
+    , player : Player
+    , cells : Array Cell
+    }
+
+type alias Editor =
+    { drawStyle : Boundary
+    }
+
+type alias Editing a =
+    { a
+    | board : Board
+    , editor : Editor
+    }
+
+type alias Programmer =
+    { moves : List Msg
+    , program : String
+    }
+
+type alias Programming a =
+    { a
+    | board : Board
+    , programmer : Programmer
+    }
+
+type alias Executor solver =
+    { solver : Maybe solver
+    , init : Solve -> solver
+    , update : solver -> ( solver, Move )
+    }
+
+type alias Executing a solver =
+    { a
+    | board : Board
+    , executor : Executor solver
+    }
+
+type alias Solve =
+    { board : Board
+    , commands : List Msg
+    }
+
+type Mode
+    = Edit
+    | Program
+    | Execute
+
+type alias Cell =
+    { cellType : CellType
+    , top : Boundary
+    , left : Boundary
+    , bottom : Boundary
+    , right : Boundary
+    }
+
+type CellType
+    = Empty
+    | Start
+    | Goal
+
+type Boundary
+    = Wall
+    | Alley
+
+type alias Player =
+    { x : Int
+    , y : Int
+    , orientation : Direction
+    }
+
+type Direction
+    = Right
+    | Up
+    | Left
+    | Down
+
+type Msg
+    = KeyArrow Direction
+    | KeyShift Bool
+    | SwitchMode Mode
+    | KeyOtherDown String
+    | KeyOtherUp String
+    | Tick Time.Posix
+    | ProgramChanged String
+
+testBoard : Board -> Board
+testBoard board =
+    board
+      |> updateCellBoundary (0, 0) Up Alley
+      |> updateCellBoundary (0, 1) Up Alley
+      |> updateCellBoundary (0, 2) Up Alley
+      |> updateCellBoundary (0, 3) Right Alley
+      |> updateCellBoundary (1, 3) Right Alley
+      |> updateCellBoundary (2, 3) Right Alley
+      |> updateCellBoundary (3, 3) Down Alley
+      |> updateCellBoundary (3, 2) Down Alley
+      |> updateCellBoundary (3, 1) Left Alley
+      |> updateCellBoundary (2, 1) Left Alley
+      |> updateCellBoundary (1, 1) Left Alley
+      |> updateCell ( 0, 0 ) (updateCellType Start)
+      |> updateCell ( 9, 9 ) (updateCellType Goal)
+
+testProgram : String
+testProgram = String.join "\n"
+  [ "forward"
+  , "right"
+  , "right"
+  ]
+
+initGame : (Solve -> solver) -> (solver -> (solver, Move)) -> ( Game solver, Cmd msg )
+initGame init update =
+    let
+        game =
+            { board = newBoard 10 10
+                |> testBoard
+                |> playerAtStart
+            , mode = Edit
+            , editor =
+                { drawStyle = Alley
+                }
+            , programmer =
+                { moves = []
+                , program = testProgram
+                }
+            , executor =
+                { solver = Nothing
+                , init = init
+                , update = update
+                }
+            }
+    in
+        ( game, Cmd.none )
+
+newBoard : Int -> Int -> Board
+newBoard width height =
+    { width = width
+    , height = height
+    , cells =
+        Array.repeat (width * height)
+            { cellType = Empty
+            , top = Wall
+            , left = Wall
+            , bottom = Wall
+            , right = Wall
+            }
+    , player = Player 0 0 Up
+    }
+
+updateGame : Msg -> Game solver -> ( Game solver, Cmd Msg )
+updateGame msg game =
+    let
+        { board, programmer, executor } = game
+
+        updatePlayer mode = if mode == Edit
+            then board
+            else playerAtStart board
+
+        updateExecutor mode = if mode == Execute
+            then { executor
+                 | solver = Just <| executor.init <| Solve board programmer.moves
+                 }
+            else { executor | solver = Nothing }
+
+        updateProgrammer mode = if mode == Program
+            then { programmer | moves = [] }
+            else programmer
+
+        updatedGame =
+            case msg of
+                ProgramChanged newProgram ->
+                  { game
+                  | programmer = { programmer | program = newProgram }
+                  }
+
+                SwitchMode mode ->
+                    { game
+                    | mode = mode
+                    , board = updatePlayer mode
+                    , executor = updateExecutor mode
+                    , programmer = updateProgrammer mode
+                    }
+
+                _ ->
+                    case game.mode of
+                        Program -> updateGameProgramMode msg game
+                        Edit    -> updateGameEditMode msg game
+                        Execute -> updateGameExecuteMode msg game
+    in
+        ( updatedGame, Cmd.none )
+
+updateGameExecuteMode : Msg -> Executing a s -> Executing a s
+updateGameExecuteMode msg game =
+    let
+        { board, executor } = game
+        { player } = board
+
+        ( updatedSolver, move ) = case executor.solver of
+           Just solver -> executor.update solver
+                            |> Tuple.mapFirst Just
+           _           -> ( Nothing, Nop )
+
+        movedPlayer = player |> case move of
+            Forward   -> movePlayer player.orientation
+            TurnLeft  -> turnPlayer Left
+            TurnRight -> turnPlayer Right
+            _         -> identity
+    in
+        case msg of
+          Tick _ -> { game
+                    | board = { board | player = movedPlayer }
+                    , executor = { executor | solver = updatedSolver }
+                    }
+          _      -> game
+
+updateGameProgramMode : Msg -> Programming a -> Programming a
+updateGameProgramMode msg game =
+    let
+        { board, programmer } = game
+        { player } = board
+        { x, y, orientation } = player
+        { moves } = programmer
+
+        blocked direction cell =
+            case direction of
+                Right -> cell.right == Wall
+                Left  -> cell.left == Wall
+                Up    -> cell.top == Wall
+                Down  -> cell.bottom == Wall
+
+        isBlocked direction =
+            queryCell ( x, y ) board (blocked direction)
+    in
+        case msg of
+            KeyArrow Up ->
+                if isBlocked orientation then
+                    game
+                else
+                    { game
+                    | board = { board | player = movePlayer orientation player }
+                    , programmer = { programmer | moves = moves ++ [ msg ] }
+                    }
+
+            KeyArrow Down -> game
+
+            KeyArrow direction ->
+                { game
+                | board = { board | player = turnPlayer direction player }
+                , programmer = { programmer | moves = moves ++ [ msg ] }
+                }
+
+            _ -> game
+
+updateGameEditMode : Msg -> Editing a -> Editing a
+updateGameEditMode msg game =
+    let
+        { board, editor } = game
+        { width, height, player } = board
+        { x, y } = player
+        { drawStyle } = editor
+
+        offBoard direction =
+            case direction of
+                Right -> x + 1 >= width
+                Left  -> x <= 0
+                Up    -> y + 1 >= height
+                Down  -> y <= 0
+
+        removeWall direction = updateCellBoundary ( x, y ) direction Alley
+        restoreWall direction = updateCellBoundary ( x, y ) direction Wall
+    in
+        case msg of
+            KeyShift True    ->
+                { game | editor = { editor | drawStyle = Wall }}
+
+            KeyShift False   ->
+                { game | editor = { editor | drawStyle = Alley }}
+
+            KeyArrow direction ->
+                let
+                    updatedBoard =
+                        if drawStyle == Wall then
+                            { board | player = movePlayer direction player }
+                                |> restoreWall Up
+                                |> restoreWall Down
+                                |> restoreWall Left
+                                |> restoreWall Right
+
+                        else { board | player = movePlayer direction player }
+                                |> removeWall direction
+                in
+                    { game | board =
+                        if offBoard direction
+                            then board
+                            else updatedBoard
+                    }
+
+            _ ->
+                game
+
+playerAtStart : Board -> Board
+playerAtStart board =
+    let
+        player = cellsWithIndex board
+            |> List.filter (\(_,_,c) -> c.cellType == Start)
+            |> List.head
+            |> Maybe.map (\(x,y,_) -> Player x y Up)
+            |> Maybe.withDefault (Player 0 0 Up)
+    in
+        { board | player = player }
+
+movePlayer : Direction -> Player -> Player
+movePlayer direction player =
+    case direction of
+        Right -> { player | x = player.x + 1 }
+        Left  -> { player | x = player.x - 1 }
+        Up    -> { player | y = player.y + 1 }
+        Down  -> { player | y = player.y - 1 }
+
+turnPlayer : Direction -> Player -> Player
+turnPlayer direction player =
+    { player | orientation =
+        case direction of
+            Right -> rightOfDirection player.orientation
+            Left  -> leftOfDirection player.orientation
+            _     -> player.orientation
+    }
+
+queryCell : ( Int, Int ) -> Board -> (Cell -> Bool) -> Bool
+queryCell ( x, y ) { width, height, cells } query =
+    Array.get ((height - 1 - y) * width + x) cells
+        |> Maybe.map query
+        |> Maybe.withDefault True
+
+updateCell : ( Int, Int ) -> (Cell -> Cell) -> Board -> Board
+updateCell ( x, y ) f board =
+    let
+        { width, height, cells } = board
+        i = (height - 1 - y) * width + x
+        update cell = { board | cells = Array.set i cell cells }
+    in
+        Array.get i cells
+            |> Maybe.map (f >> update)
+            |> Maybe.withDefault board
+
+updateCellType : CellType -> Cell -> Cell
+updateCellType t c = { c | cellType = t }
+
+updateCellBoundary : ( Int, Int ) -> Direction -> Boundary -> Board -> Board
+updateCellBoundary ( x, y ) direction boundary board =
+    let
+        update dir cell =
+            case dir of
+                Up    -> { cell | top = boundary }
+                Down  -> { cell | bottom = boundary }
+                Left  -> { cell | left = boundary }
+                Right -> { cell | right = boundary }
+
+        neighbour =
+            case direction of
+                Up    -> ( x, y + 1 )
+                Down  -> ( x, y - 1 )
+                Left  -> ( x - 1, y )
+                Right -> ( x + 1, y )
+    in
+    board
+        |> updateCell ( x, y ) (update direction)
+        |> updateCell neighbour (update <| oppositeDirection direction)
+
+viewGame : Game solver -> List (Html Msg)
+viewGame game =
+    let
+        { board, programmer } = game
+        { player } = board
+
+        playerAt ( x, y ) =
+            if (player.x, player.y) == (x, y)
+                then Just player.orientation
+                else Nothing
+
+        render ( x, y, cell ) = viewCell ( x, y ) (playerAt ( x, y )) cell
+    in
+        [ cellsWithIndex board
+            |> List.map render
+            |> group
+            |> svg
+        , Html.div []
+            [ Html.button
+                [ onClick <| SwitchMode Edit ]
+                [ Html.text "edit" ]
+            , Html.button
+                [ onClick <| SwitchMode Program ]
+                [ Html.text "program" ]
+            , Html.button
+                [ onClick <| SwitchMode Execute ]
+                [ Html.text "execute" ]
+            ]
+        , Html.div []
+            [ Html.textarea
+                [ Atts.cols 40
+                , Atts.rows 30
+                , Atts.value <| toProgram programmer.moves
+                ]
+                [ ]
+            , Html.textarea
+                [ Atts.cols 40
+                , Atts.rows 30
+                , onInput ProgramChanged
+                ]
+                [ Html.text programmer.program ]
+            ]
+        ]
+
+toProgram : List Msg -> String
+toProgram messages =
+    let
+        toCommand msg = case msg of
+           KeyArrow Up    -> "forward"
+           KeyArrow Left  -> "left"
+           KeyArrow Right -> "right"
+           _              -> ""
+    in
+        messages
+            |> List.map toCommand
+            |> String.join "\n"
+
+oppositeDirection : Direction -> Direction
+oppositeDirection d =
+    case d of
+        Up    -> Down
+        Down  -> Up
+        Left  -> Right
+        Right -> Left
+
+rightOfDirection : Direction -> Direction
+rightOfDirection d =
+    case d of
+        Right -> Down
+        Up    -> Right
+        Left  -> Up
+        Down  -> Left
+
+leftOfDirection : Direction -> Direction
+leftOfDirection d =
+    case d of
+        Right -> Up
+        Up    -> Left
+        Left  -> Down
+        Down  -> Right
+
+cellsWithIndex : Board -> List ( Int, Int, Cell )
+cellsWithIndex { width, height, cells } =
+    cells
+        |> Array.indexedMap
+            (\i c ->
+                ( modBy width i, height - 1 - i // width, c )
+            )
+        |> Array.toList
+
+
+viewCell : ( Int, Int ) -> Maybe Direction -> Cell -> Collage Msg
+viewCell ( x, y ) direction { cellType, left, top, bottom, right } =
+    let
+        wallStyle wall =
+            case wall of
+                Wall  -> solid thin (uniform black)
+                Alley -> invisible
+
+        cell =
+            case cellType of
+                Empty -> []
+
+                Start ->
+                    [ Text.fromString "S"
+                        |> Text.color red
+                        |> Text.weight Text.Bold
+                        |> Text.size Text.huge
+                        |> rendered
+                    ]
+
+                Goal ->
+                    [ Text.fromString "G"
+                        |> Text.color green
+                        |> Text.weight Text.Bold
+                        |> Text.size Text.huge
+                        |> rendered
+                    ]
+
+        angle d =
+            case d of
+                Left  -> -pi / 2
+                Up    -> pi
+                Right -> pi / 2
+                Down  -> 0
+
+        player =
+            case direction of
+                Nothing -> []
+
+                Just d ->
+                    [ Text.fromString "T"
+                        |> Text.color blue
+                        |> Text.weight Text.Bold
+                        |> Text.size Text.huge
+                        |> rendered
+                        |> rotate (angle d)
+                    ]
+    in
+        group
+            [ line 50
+                |> traced (wallStyle bottom)
+                |> shiftY -25
+            , line 50
+                |> traced (wallStyle top)
+                |> shiftY 25
+            , line 50
+                |> traced (wallStyle right)
+                |> rotate (pi / 2)
+                |> shiftX 25
+            , line 50
+                |> traced (wallStyle left)
+                |> rotate (pi / 2)
+                |> shiftX -25
+            , group
+                player
+            , group
+                cell
+            , square 50
+                |> filled (uniform lightYellow)
+            ]
+            |> shift ( 50 * toFloat x, 50 * toFloat y )
+
+keyDownDecoder : Decode.Decoder Msg
+keyDownDecoder =
+    let
+        toDirection string =
+            case string of
+                "ArrowLeft"  -> KeyArrow Left
+                "ArrowRight" -> KeyArrow Right
+                "ArrowUp"    -> KeyArrow Up
+                "ArrowDown"  -> KeyArrow Down
+                "Shift"      -> KeyShift True
+                "E"          -> SwitchMode Edit
+                "e"          -> SwitchMode Edit
+                "P"          -> SwitchMode Program
+                "p"          -> SwitchMode Program
+                "R"          -> SwitchMode Execute
+                "r"          -> SwitchMode Execute
+                _            -> KeyOtherDown string
+    in
+        Decode.field "key" Decode.string
+            |> Decode.map toDirection
+
+keyUpDecoder : Decode.Decoder Msg
+keyUpDecoder =
+    let
+        toDirection string =
+            case string of
+                "Shift" -> KeyShift False
+                _       -> KeyOtherUp string
+    in
+        Decode.field "key" Decode.string
+            |> Decode.map toDirection
+
+play :
+  { init : Solve -> solver
+  , update : solver -> ( solver, Move )
+  }
+  -> Program () (Game solver) Msg
+play { init, update } =
+    document
+        { subscriptions =
+            \_ -> Sub.batch
+                    [ onKeyDown keyDownDecoder
+                    , onKeyUp keyUpDecoder
+                    , Time.every 400 Tick
+                    ]
+        , init = \_ -> initGame init update
+        , update = updateGame
+        , view =
+            \game ->
+                { title = "ElMaze"
+                , body = viewGame game
+                }
+        }
+

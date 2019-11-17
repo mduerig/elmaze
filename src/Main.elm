23f1045e41e2987acab1f5429b5a13b85a72b81d
Michael Dürig <michid@gmail.com>
2019-11-17 13:04:41 +0100

    Simplify

--- src/Main.elm
+++ src/Main.elm
@@ -28,31 +28,20 @@ testBoard = newBoard 5 4
 
 updateGame : Board -> Maybe I.Interpreter -> ( Maybe I.Interpreter, Move )
 updateGame board interpreter =
-    let
-        ( newInterpreter, move ) =
-            case interpreter of
-                Just interp
-                    -> I.update board interp
-                        |> Tuple.mapFirst Just
-                _
-                    ->  ( Nothing, Nop )
-    in
-        ( newInterpreter, move )
+    case interpreter of
+        Just interp
+            -> I.update board interp
+                |> Tuple.mapFirst Just
+        _
+            ->  ( Nothing, Nop )
 
 initGame : Board -> String -> Maybe I.Interpreter
 initGame board program =
-    let
-        parsed : Result (List Parser.DeadEnd) P.Program
-        parsed = Parser.run P.program program
-
-        interpreter : Maybe I.Interpreter
-        interpreter = case parsed of
-            Ok ast
-                -> Just <| I.init ast
-            Err error
-                -> Debug.log (Debug.toString error) Nothing
-    in
-        interpreter
+    case Parser.run P.program program of
+        Ok ast
+            -> Just <| I.init ast
+        Err error
+            -> Debug.log (Debug.toString error) Nothing
 
 main : Program () (Game ( Maybe I.Interpreter )) Msg
 main =
